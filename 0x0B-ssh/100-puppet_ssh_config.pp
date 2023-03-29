4e6a8735ef885d15da7d91ba5c9b2fe9d915e76a
rcezea <rclancing@gmail.com>
2023-03-29 21:25:29 +0100

    ssh tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# set up your client SSH configuration file so that
+# you can connect to a server without typing a password
+
+exec { 'echo':
+  path    => '/usr/bin:/bin',
+  command => 'echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config',
+}
