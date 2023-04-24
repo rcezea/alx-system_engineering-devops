0933d3c50c411e1b3dcfe7caab3ff5f15f0e83e1
rcezea <rclancing@gmail.com>
2023-04-24 18:17:37 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,16 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
+    """
+    Using REST API, return info about employee todo list progress
+    """
 
     url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+
     name = requests.get(url).json().get('name')
+
     url = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+
     total = requests.get(url).json()
     done = 0
 
@@ -18,8 +24,8 @@ if __name__ == '__main__':
         if i.get('completed'):
             done += 1
 
-    print("Employee {} is done with tasks({}/{}):\
-          ".format(name, done, len(total)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done, len(total)))
 
     for i in total:
         if i.get('completed'):
