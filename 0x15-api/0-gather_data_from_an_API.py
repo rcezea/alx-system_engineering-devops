5586512239510b12178162fbdfc81a1f0aefa63b
rcezea <rclancing@gmail.com>
2023-04-24 17:57:02 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Using REST API, return info about employee todo list progress
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    name = requests.get(url).json().get('name')
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+    total = requests.get(url).json()
+    done = 0
+
+    for i in total:
+        if i.get('completed'):
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):\
+          ".format(name, done, len(total)))
+
+    for i in total:
+        if i.get('completed'):
+            print("\t{}".format(i.get('title')))
