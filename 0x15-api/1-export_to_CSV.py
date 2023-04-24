776250b678760be0f476704d30f6a4c51bd654ee
rcezea <rclancing@gmail.com>
2023-04-24 23:40:38 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
 """
-Module well documented
+Using REST API, return info about employee todo list progress
 """
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+
+    username = requests.get(url, timeout=10)
+
+    username = username.json()
+
+    username = username.get('username')
+
+    url2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            sys.argv[1])
+
+    total = requests.get(url2, timeout=10)
+
+    total = total.json()
+    done = 0
+
+    with open("USER_ID.csv", "w") as csvfile:
+        for i in total:
+            status = i.get('completed')
+            title = i.get('title')
+            data = [sys.argv[1], username, status, title]
+            write = csv.writer(csvfile)
+            write.writerow(data)
