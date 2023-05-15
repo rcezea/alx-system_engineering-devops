379a006e477e8a321cac639cca51149fe92ef6bd
Ezea Richard <rclancing@gmail.com>
2023-05-15 12:40:13 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Gets number of subscribers in a subreddit
-"""
+"""A module that returns the number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Gets number of subscribers in a subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Ezea Richard'}
-    r = requests.get(url, headers=headers).json()
-    # if r.status_code != 200:
-    #     return 0
-    return (r.get("data", {}).get("subscribers", 0))
+    """Returns the number of subscribers"""
+    headers = {'X-Modhash': 'Alx student subscribers',
+               'User-Agent': 'Alx subscriber task v1.01'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        return 0
+    return resp.json()['data']['subscribers']
