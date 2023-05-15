5718d6492a589074a9dfbb0ea204601161ddd14a
Ezea Richard <rclancing@gmail.com>
2023-05-15 15:05:21 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,6 +11,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Ezea Richard'}
     link = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 10}
-    r = req.get(url=link, headers=headers, params=params).json()
-    for i in (r.get("data", {}).get("children", 0)):
-        print(i.get("data", {}).get("title", 0))
+    q = req.get(url=link, headers=headers, params=params)
+    r = q.json()['data']['children']
+    for i in r:
+        print(i["data"]["title"])
