5d54a4237aa6fc600dd3b1bcb703450288cbb4b4
Ezea Richard <rclancing@gmail.com>
2023-05-15 13:52:10 +0100

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" prints the titles of the first 10 hot posts listed for a given subreddit """
+
+import requests as req
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed for a given subreddit """
+    headers = {'User-Agent': 'Ezea Richard'}
+    link = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'limit': 10}
+    r = req.get(url=link, headers=headers, params=params).json()
+    for i in (r.get("data", {}).get("children", 0)):
+        print(i.get("data").get("title"))
