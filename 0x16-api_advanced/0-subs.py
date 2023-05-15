af6c040d85d653dfe94bfe13f378f3f150763859
Ezea Richard <rclancing@gmail.com>
2023-05-15 12:37:54 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Gets number of subscribers in a subreddit """
+"""
+Gets number of subscribers in a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Gets number of subscribers in a subreddit """
+    """
+    Gets number of subscribers in a subreddit
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Ezea Richard'}
     r = requests.get(url, headers=headers).json()
